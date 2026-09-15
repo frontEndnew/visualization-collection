0f8b018bbe9d32b907eaccf5dc2d05d42877ba7c
lishulincug <lishulincug@users.noreply.github.com>
2026-09-15 06:01:01 +0000

    deploy: 0ead9a222bb86d7c64bf6969a3f7247313365759

--- src/pages/css/MusicVisualization/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ComingSoon from "@/components/ComingSoon";
-
-const MusicVisualization = () => {
-  return <ComingSoon />;
-};
-
-export default MusicVisualization;
--- src/pages/html/VisualDesign/components/BgFusion/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./index.module.scss";
-
-const BgFusion = () => {
-  return <div className={styles.container} />;
-};
-
-export default BgFusion;
